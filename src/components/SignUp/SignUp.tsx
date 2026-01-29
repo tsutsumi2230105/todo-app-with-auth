a0d4c05eba375c1aa212cb153b771b3c9f0b0dce
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-01-29 17:49:11 +0900

    chore:JSXの修正

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -3,7 +3,7 @@ import "./SignUp.scss"
 
 const SignUp = () => {
   return (
-    <div className="signup__page">
+    <div className="signup">
       <div className="signup__component">
         <div className="signup__heading">
           <h1 className="signup__title">サインアップ</h1>
@@ -13,57 +13,61 @@ const SignUp = () => {
         </div>
 
         <form className="signup__form">
-          <div className="signup__field">
-            <label className="signup__label" htmlFor="email">
-              メールアドレス
-            </label>
-            <input
-              className="signup__input"
-              placeholder="email@example.com"
-              id="email"
-              name="email"
-              type="email"
-            ></input>
-          </div>
+          <div className="login__main">
+            <div className="signup__fields">
+              <div className="signup__field">
+                <label className="signup__label" htmlFor="email">
+                  メールアドレス
+                </label>
+                <input
+                  className="signup__input"
+                  placeholder="email@example.com"
+                  id="email"
+                  name="email"
+                  type="email"
+                />
+              </div>
 
-          <div className="signup__field">
-            <label className="signup__label" htmlFor="password">
-              パスワード
-            </label>
-            <input
-              className="signup__input"
-              placeholder="password"
-              id="password"
-              name="password"
-              type="password"
-            ></input>
-          </div>
+              <div className="signup__field">
+                <label className="signup__label" htmlFor="password">
+                  パスワード
+                </label>
+                <input
+                  className="signup__input"
+                  placeholder="password"
+                  id="password"
+                  name="password"
+                  type="password"
+                ></input>
+              </div>
 
-          <div className="signup__field">
-            <label className="signup__label" htmlFor="password__confirm">
-              パスワード確認
-            </label>
-            <input
-              className="signup__input"
-              placeholder="password"
-              id="password__confirm"
-              name="password__confirm"
-              type="password"
-            ></input>
+              <div className="signup__field">
+                <label className="signup__label" htmlFor="password__confirm">
+                  パスワード確認
+                </label>
+                <input
+                  className="signup__input"
+                  placeholder="password"
+                  id="password__confirm"
+                  name="password__confirm"
+                  type="password"
+                ></input>
+              </div>
+
+              <div className="signup__action">
+                <button className="signup__button" type="submit">
+                  サインアップ
+                </button>
+                <p>
+                  すでにアカウントをお持ちの方は
+                  <Link to="/login" className="signup__link">
+                    ログイン
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
         </form>
-
-        <div className="signup__action">
-          <button className="signup__button" type="submit">
-            サインアップ
-          </button>
-          <p>
-            すでにアカウントをお持ちの方は
-            <Link to="/login" className="signup__link">
-              ログイン
-            </Link>
-          </p>
-        </div>
       </div>
     </div>
   )
