f22a2e34025a6cce7446ac41359fe3c635eb1eb5
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-01-29 17:33:37 +0900

    feat:signupへのルーティングを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import SignUp from "./components/SignUp/SignUp"
 import Login from "./components/Login/Login"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   )
