96e342f945a26f7d83fc9a64e26a6cfea4ed5ffa
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-01-29 17:04:57 +0900

    feat:ルーティングの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom"
 import Login from "./components/Login"
 
 function App() {
-  return <Login />
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
