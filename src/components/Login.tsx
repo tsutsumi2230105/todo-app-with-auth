712ce2274ec3cdea5e60effbb43e7abde5a5baf1
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-01-29 13:01:33 +0900

    chore:buttonをform内に移動

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,44 +11,48 @@ const Login = () => {
         </div>
 
         <form className="login__form">
-          <div className="login__field">
-            <label className="login__label" htmlFor="email">
-              メールアドレス
-            </label>
-            <input
-              className="login__input"
-              placeholder="email@example.com"
-              id="email"
-              name="email"
-              type="email"
-            />
-          </div>
+          <div className="login__main">
+            <div className="login__fields">
+              <div className="login__field">
+                <label className="login__label" htmlFor="email">
+                  メールアドレス
+                </label>
+                <input
+                  className="login__input"
+                  placeholder="email@example.com"
+                  id="email"
+                  name="email"
+                  type="email"
+                />
+              </div>
 
-          <div className="login__field">
-            <label className="login__label" htmlFor="password">
-              パスワード
-            </label>
-            <input
-              className="login__input"
-              placeholder="password"
-              id="password"
-              name="password"
-              type="password"
-            />
+              <div className="login__field">
+                <label className="login__label" htmlFor="password">
+                  パスワード
+                </label>
+                <input
+                  className="login__input"
+                  placeholder="password"
+                  id="password"
+                  name="password"
+                  type="password"
+                />
+              </div>
+            </div>
+
+            <div className="login__action">
+              <button className="login__button" type="submit">
+                ログイン
+              </button>
+              <p>
+                アカウントをお持ちでない方は
+                <Link to="/signup" className="login__link">
+                  サインアップ
+                </Link>
+              </p>
+            </div>
           </div>
         </form>
-
-        <div className="login__action">
-          <button className="login__button" type="submit">
-            ログイン
-          </button>
-          <p>
-            アカウントをお持ちでない方は
-            <Link to="/signup" className="login__link">
-              サインアップ
-            </Link>
-          </p>
-        </div>
       </div>
     </div>
   )
