1f5321f1b0328e186ccfbd050eea44678c89d9db
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-02 14:48:42 +0900

    refactor:StatusCard コンポーネント側でiconの出し分けを一元化。

--- src/Pages/DashBoard/index.tsx
+++ src/Pages/DashBoard/index.tsx
@@ -1,38 +1,14 @@
+import StatusCard from "../../components/dashboard/StatusCard/"
 import "./Dashboard.scss"
-import StatusCard from "../../components/dashboard/StatusCard"
-import AllTaskLogo from "../../assets/images/all_task_logo.png"
-import ActiveTaskLogo from "../../assets/images/active_task_logo.png"
-import CompletedLogo from "../../assets/images/completed_task_logo.png"
-import ExpiredLogo from "../../assets/images/expired_task_logo.png"
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <div className="statuscards">
-        <StatusCard
-          label="総タスク数"
-          value={5}
-          iconSrc={AllTaskLogo}
-          variant="all"
-        />
-        <StatusCard
-          label="進行中"
-          value={3}
-          iconSrc={ActiveTaskLogo}
-          variant="active"
-        />
-        <StatusCard
-          label="完了済み"
-          value={2}
-          iconSrc={CompletedLogo}
-          variant="done"
-        />
-        <StatusCard
-          label="期限切れ"
-          value={0}
-          iconSrc={ExpiredLogo}
-          variant="expired"
-        />
+        <StatusCard label="総タスク数" value={5} variant="all" />
+        <StatusCard label="進行中" value={3} variant="active" />
+        <StatusCard label="完了済み" value={2} variant="done" />
+        <StatusCard label="期限切れ" value={0} variant="expired" />
       </div>
     </div>
   )
--- src/components/dashboard/StatusCard/index.tsx
+++ src/components/dashboard/StatusCard/index.tsx
@@ -1,20 +1,26 @@
 import "./StatusCard.scss"
+import AllTaskLogo from "../../../assets/images/all_task_logo.png"
+import ActiveTaskLogo from "../../../assets/images/active_task_logo.png"
+import CompletedLogo from "../../../assets/images/completed_task_logo.png"
+import ExpiredLogo from "../../../assets/images/expired_task_logo.png"
 
 type Variant = "all" | "active" | "done" | "expired"
 
 type DashBoardStatusCardProps = {
   label: string
   value: number
-  iconSrc: string
   variant: Variant
 }
 
-const StatusCard = ({
-  label,
-  value,
-  iconSrc,
-  variant,
-}: DashBoardStatusCardProps) => {
+const IconMap: Record<Variant, string> = {
+  all: AllTaskLogo,
+  active: ActiveTaskLogo,
+  done: CompletedLogo,
+  expired: ExpiredLogo,
+}
+
+const StatusCard = ({ label, value, variant }: DashBoardStatusCardProps) => {
+  const iconSrc = IconMap[variant]
   return (
     <div className="statuscard">
       <div className="statuscard__content">
