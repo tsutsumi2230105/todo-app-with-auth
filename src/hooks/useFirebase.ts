7cbb6fe33170ae302f42e050c89edb4696828e8f
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-02 16:05:51 +0900

    feat:カスタムフックの作成

---
+++ src/hooks/useFirebase.ts
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../utils/firebase"
+
+type UseFirebase = () => {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  email: string
+  setEmail: React.Dispatch<React.SetStateAction<string>>
+  password: string
+  setPassword: React.Dispatch<React.SetStateAction<string>>
+  handleLogin: (e: React.FormEvent<HTMLFormElement>) => Promise<void>
+}
+
+export const useFirebase: UseFirebase = () => {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      const userLogin = await signInWithEmailAndPassword(auth, email, password)
+      alert("ログインに成功しました。")
+      console.log("ログインに成功しました。", userLogin)
+      navigate("/dashboard")
+    } catch (error) {
+      alert("ログインに失敗しました。")
+      console.error("ログインに失敗しました。", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    loading,
+    setLoading,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+  }
+}
