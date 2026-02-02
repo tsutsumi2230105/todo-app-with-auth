b4895828f1985a03e0a8f132e89bc7c57bbc6c55
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-02 16:14:37 +0900

    feat:ログイン機能の作成

--- src/components/auth/SubmitButton/index.tsx
+++ src/components/auth/SubmitButton/index.tsx
@@ -2,12 +2,13 @@ import "./SubmitButton.scss"
 
 type SubmitButtonProps = {
   children: React.ReactNode
+  isLoading: boolean
 }
 
-const SubmitButton = ({ children }: SubmitButtonProps) => {
+const SubmitButton = ({ children, isLoading }: SubmitButtonProps) => {
   return (
-    <button className="submit-button" type="submit">
-      {children}
+    <button className="submit-button" type="submit" disabled={isLoading}>
+      {isLoading ? "ログイン中..." : children}{" "}
     </button>
   )
 }
