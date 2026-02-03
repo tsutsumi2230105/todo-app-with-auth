d2e59da76cbbd60098c002b99583066691b93fbf
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-03 16:37:37 +0900

    style:DasboardページのHeader作成とスタイリング。

--- src/Pages/DashBoard/index.tsx
+++ src/Pages/DashBoard/index.tsx
@@ -7,15 +7,22 @@ const Dashboard = () => {
   const { handleLogout } = useAuth()
   return (
     <div className="dashboard">
-      <div className="dashboard_logout">
-        <img src={LogoutIcon} alt="ログアウトアイコン" />
-        <button onClick={handleLogout}>ログアウト</button>
-      </div>
-      <div className="statuscards">
-        <StatusCard label="総タスク数" value={5} variant="all" />
-        <StatusCard label="進行中" value={3} variant="active" />
-        <StatusCard label="完了済み" value={2} variant="done" />
-        <StatusCard label="期限切れ" value={0} variant="expired" />
+      <div className="dashboard_contents">
+        <header className="dashboard_header">
+          <div className="dashboard_title">
+            <h1>TODOリスト</h1>
+          </div>
+          <div className="dashboard_logout">
+            <img src={LogoutIcon} alt="ログアウトアイコン" />
+            <button onClick={handleLogout}>ログアウト</button>
+          </div>
+        </header>
+        <div className="statuscards">
+          <StatusCard label="総タスク数" value={5} variant="all" />
+          <StatusCard label="進行中" value={3} variant="active" />
+          <StatusCard label="完了済み" value={2} variant="done" />
+          <StatusCard label="期限切れ" value={0} variant="expired" />
+        </div>
       </div>
     </div>
   )
