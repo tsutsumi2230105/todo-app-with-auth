05a0464224dcaab1c6e955cbc10c07a498bf2480
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-04 17:54:39 +0900

    chore:不要なフラグメントの削除

--- src/Pages/DashBoard/index.tsx
+++ src/Pages/DashBoard/index.tsx
@@ -7,38 +7,36 @@ import { useAuth } from "../../hooks/useAuth"
 const Dashboard = () => {
   const { handleLogout } = useAuth()
   return (
-    <>
-      <div className="dashboard">
-        <div className="dashboard__contents">
-          <header className="dashboard__header">
-            <h1 className="dashboard__title">TODOリスト</h1>
-            <div className="dashboard__logout">
-              <button onClick={handleLogout}>
-                <img src={LogoutIcon} alt="ログアウトアイコン" />
-                <span>ログアウト</span>
-              </button>
-            </div>
-          </header>
-          <div className="dashboard__statuscard">
-            <div className="statuscards">
-              <StatusCard label="総タスク数" value={5} variant="all" />
-              <StatusCard label="進行中" value={3} variant="active" />
-              <StatusCard label="完了済み" value={2} variant="done" />
-              <StatusCard label="期限切れ" value={0} variant="expired" />
-            </div>
+    <div className="dashboard">
+      <div className="dashboard__contents">
+        <header className="dashboard__header">
+          <h1 className="dashboard__title">TODOリスト</h1>
+          <div className="dashboard__logout">
+            <button onClick={handleLogout}>
+              <img src={LogoutIcon} alt="ログアウトアイコン" />
+              <span>ログアウト</span>
+            </button>
           </div>
-          <div className="dashboard__todo">
-            <div className="todorest">
-              <p>5件のタスクを表示中</p>
-            </div>
-            <div className="todos">
-              <ToDo label="ReactRouterを学ぶ" />
-              <ToDo label="テスト" />
-            </div>
+        </header>
+        <div className="dashboard__statuscard">
+          <div className="statuscards">
+            <StatusCard label="総タスク数" value={5} variant="all" />
+            <StatusCard label="進行中" value={3} variant="active" />
+            <StatusCard label="完了済み" value={2} variant="done" />
+            <StatusCard label="期限切れ" value={0} variant="expired" />
+          </div>
+        </div>
+        <div className="dashboard__todo">
+          <div className="todorest">
+            <p>5件のタスクを表示中</p>
+          </div>
+          <div className="todos">
+            <ToDo label="ReactRouterを学ぶ" />
+            <ToDo label="テスト" />
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 export default Dashboard
