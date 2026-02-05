4577d180955c7a80ee5631c757ed3ae37a75357c
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-05 11:30:08 +0900

    practice

--- src/components/dashboard/AddToDo/index.tsx
+++ src/components/dashboard/AddToDo/index.tsx
@@ -1,47 +1,54 @@
 import "./AddToDo.scss"
-import { useId, useState } from "react"
 
-type DashBoardToDoProps = {
-  label?: string
-}
-
-const AddToDo = ({ label }: DashBoardToDoProps) => {
-  const [] = useState(false)
-  const checkboxId = useId()
-
+const AddToDo = () => {
   return (
-    <>
-      <div className="add-todo__field">
+    <div className="add-todo__form">
+      <div className="add-todo__header">
         <div className="add-todo__title">
           <h2>新しいTODOを追加</h2>
         </div>
         <div className="add-todo__sub-title">
           <h3>タスクの詳細を入力してください</h3>
         </div>
+      </div>
 
-        <div className="add-todo__form">
-          <div className="add-todo-form__field">
-            <label htmlFor="add-todo__title">タイトル</label>
-            <input id="add-todo__title" type="text" placeholder="TODOを入力…" />
-            <div />
+      <div className="add-todo__main">
+        <div className="add-todo__form--fields">
+          <div className="add-todo__form--field">
+            <label htmlFor="add-todo__form--title">タイトル</label>
+            <input
+              id="add-todo__form--title"
+              type="text"
+              placeholder="TODOを入力…"
+              className="input__form"
+            />
+          </div>
+          <div className="add-todo__form--field">
+            <label htmlFor="add-todo__form--limit">期限</label>
+            <input
+              id="add-todo__form--limit"
+              type="date"
+              className="input__form"
+            />
+          </div>
+        </div>
 
-            <label htmlFor="add-todo__limit">期限</label>
-            <input id="add-todo__limit" type="date" />
-            <label htmlFor="add-todo__priority">優先度</label>
-            <select id="add-todo__priority">
+        <div className="add-todo__form--fields">
+          <div className="add-todo__form--field">
+            <label htmlFor="add-todo__form--priority">優先度</label>
+            <select id="add-todo__form--priority" className="input__form">
               <option value="all">すべて</option>
               <option value="high">高</option>
               <option value="middle">中</option>
               <option value="low">低</option>
             </select>
-            <div className="">
-              <button>+ 追加</button>
-            </div>
-            <div />
+          </div>
+          <div className="add-todo__form--field">
+            <button>+ 追加</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
