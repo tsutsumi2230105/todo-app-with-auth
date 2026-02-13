136805595a058c74c3084f717da1a671b13eabab
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-13 09:42:57 +0900

    feat:ルートガードと、トーストを使ったエラーハンドリングの実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import { Route, Routes } from "react-router-dom"
 import SignUp from "./Pages/SignUp"
 import Login from "./Pages/Login"
 import Dashboard from "./Pages/DashBoard"
+import { Toaster } from "react-hot-toast"
+import PrivateRoute from "./components/routing"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
   )
 }
 
