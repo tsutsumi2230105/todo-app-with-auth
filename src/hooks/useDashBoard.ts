ba98d3a04b501ea49718ce908397df6a2e8e77af
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-13 14:42:54 +0900

    chore:ToDoの順番を降順に修正。

--- src/hooks/useDashBoard.ts
+++ src/hooks/useDashBoard.ts
@@ -67,7 +67,7 @@ export const useDashBoard = () => {
     if (!user) return
     const todosRef = collection(db, "users", user.uid, "todos")
 
-    const sortQuery = query(todosRef, orderBy("createdAt", "asc"))
+    const sortQuery = query(todosRef, orderBy("createdAt", "desc"))
 
     const unsubscribe = onSnapshot(sortQuery, (snapshot) => {
       const todosData = snapshot.docs.map((doc) => ({
