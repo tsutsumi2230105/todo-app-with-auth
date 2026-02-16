0c0d71bee40be2c72943f67df2bf42030f29989a
堤健悟 <tsutsumikengo@tsutsumikengonoMac-mini.local>
2026-02-16 19:16:23 +0900

    feat:トップページにアクセスした際、ログイン済みのユーザーはDashboardへ遷移するように変更。

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import SignUp from "./Pages/SignUp"
 import Login from "./Pages/Login"
 import Dashboard from "./Pages/DashBoard"
 import { Toaster } from "react-hot-toast"
 import ProtectedRoute from "./components/ProtectedRoute"
-import { Navigate } from "react-router-dom"
+import { useAuth } from "./hooks/useAuth"
 
 function App() {
+  const { user, loading } = useAuth()
+  if (loading) return null
   return (
     <>
       <Toaster position="top-right" />
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={
+            user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
